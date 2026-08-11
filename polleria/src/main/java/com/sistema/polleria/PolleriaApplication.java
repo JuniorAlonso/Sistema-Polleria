2ea224af0362cda7015d84dcc3bd1aa73a77bca7
JuniorAlonso <alonsocb56@gmail.com>
2026-08-10 21:53:10 -0500

    Subiendo paquetes

---
+++ polleria/src/main/java/com/sistema/polleria/PolleriaApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.polleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolleriaApplication.class, args);
+	}
+
+}
---
+++ polleria/src/test/java/com/sistema/polleria/PolleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.polleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
